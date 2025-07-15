517ee33027cbfe806daf9154550f72f17bc9d03c
SeanLampa <Sean@blinkvoice.com>
2025-07-15 17:15:02 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/', async (req, res) => {
+app.post('/lease-request', async (req, res) => {
   try {
     const payload = req.body;
     const apiKey = 'ee187c52-f3da-4c62-8d5f-8d01848e22c0';
     const vendorId = '516279.8023';
+
     payload.vendorId = vendorId;
 
     const response = await fetch('https://testdevportal.marlincapitalsolutions.com:8077/ws/rest/partnerefapi/createPartnerEfApi/', {
@@ -27,6 +28,8 @@ app.post('/', async (req, res) => {
     res.status(500).send(err.toString());
   }
 });
+
+app.all('*', (req, res) => res.status(404).send('Not Found'));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Proxy listening on ${port}`));
